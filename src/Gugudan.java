aabd569d2b63562a9bf4b350932b8ec50951d9f4
yxxiin <12203369@inha.edu>
2022-09-08 18:07:14 +0900

    method

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
 
 public class Gugudan {
-    public static void main(String[] args) {
-        //2-9단 전체 출력
+    public static int[] calculate(int times){
         int[] result = new int[9];
 
-        for(int j = 2;j < 10;j++){
-            for(int i = 0;i < result.length;i++){
-                result[i] = j*(i+1);
-                System.out.println(result[i]);
-            }
+        for(int i = 0; i < result.length;i++){
+            result[i] = times * (i+1);
+        }
+
+        return result;
+    }
+    public static void print(int[] result){
+        for(int i = 0;i < result.length;i++){
+            System.out.println(result[i]);
+        }
+    }
+    public static void main(String[] args) {
+        for(int j = 2;j < 10; j++){
+            int[] result = calculate(j);
+            print(result);
         }
     }
 }
