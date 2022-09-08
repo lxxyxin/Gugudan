aecb691afef8dfdc3f1ae15050d9e173e94c7469
yxxiin <12203369@inha.edu>
2022-09-08 17:45:39 +0900

    array

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -2,19 +2,15 @@ import java.util.Scanner;
 
 public class Gugudan {
     public static void main(String[] args) {
-        System.out.println("구구단 중 출력할 단은? : ");
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
+        //2-9단 전체 출력
+        int[] result = new int[9];
 
-       if (number < 2) {
-           System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
-       } else if (number > 9) {
-           System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
-       } else {
-           for (int i = 1; i < 10; i++){
-               System.out.println(number * i);
-           }
-       }
+        for(int j = 2;j < 10;j++){
+            for(int i = 0;i < result.length;i++){
+                result[i] = j*(i+1);
+                System.out.println(result[i]);
+            }
+        }
     }
 }
 
